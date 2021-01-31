6a22f9d3db016493c1b3da3ab62df8a882433ebd
Spider <massoudfataou@gmail.com>
2021-01-31 03:18:09 +0100

    Modification de la fonction de recherche.
    Signed-off-by: Spider <massoudfataou@gmail.com>

--- src/js/components/Ingredient.jsx
+++ src/js/components/Ingredient.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 
+/**
+ *
+ * @param ingredient Infos sur l'ingrédient
+ * @param filterText Le texte recherché : Pour afficher les caractères surlignés
+ * @returns {JSX.Element}
+ */
 const Ingredient = ({ingredient, filterText=""}) => {
 	let startingIndex = ingredient.name.indexOf(filterText)
 	let endingIndex = ingredient.name.indexOf(filterText) + filterText.length
@@ -9,20 +15,21 @@ const Ingredient = ({ingredient, filterText=""}) => {
 }
 
 const IngredientTable = ({ingredients, filterText}) => {
-	if (filterText.length >= 3){
-		let filteredIngredients = ingredients.filter(ingredient => ingredient.name.indexOf(filterText) !== -1);
-		console.log(filteredIngredients);
-		return filteredIngredients.length > 0
-			?
-			<div className="d-flex flex-row flex-wrap">
-				{filteredIngredients.map(ingredient => <Ingredient filterText={filterText} key={ingredient.id} ingredient={ingredient}/>) }
-			</div>
-			:
-			<p>Aucun résultat pour votre recherche</p>
-	}
-	else {
-		return <div>Bientôt la fonctionnalité d'ingrédients les plus utilisés</div>
-	}
+
+	// On fait le trie des ingrédients selon la taille de la chaine de recherche
+	let filteredIngredients = filterText.length >= 3
+		?
+		ingredients.filter(ingredient => ingredient.name.indexOf(filterText) !== -1)
+		:
+		ingredients.filter(ingredient => filterText !== "" && ingredient.name.startsWith(filterText));
+
+	return filteredIngredients.length > 0
+		?
+		<div className="d-flex flex-row flex-wrap">
+			{filteredIngredients.map(ingredient => <Ingredient filterText={filterText} key={ingredient.id} ingredient={ingredient}/>) }
+		</div>
+		:
+		<p>Aucun résultat pour votre recherche</p>
 }
 
 export default IngredientTable
